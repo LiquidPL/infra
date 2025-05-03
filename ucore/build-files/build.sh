8cb39200e4465195fa4d173cb199444344a410ce
Krzysztof Gutkowski <krzysio.gutkowski@gmail.com>
2025-05-04 00:13:59 +0200

    feat(ucore): add config for building vm images

--- ucore/build-files/build.sh
+++ ucore/build-files/build.sh
@@ -14,3 +14,29 @@ rpm-ostree install -y /ctx/built-kmods/*.rpm \
 
 rm /etc/yum.repos.d/_copr-gasket.repo \
    /etc/yum.repos.d/rancher-k3s-common.repo
+
+# TODO: remove this once bootc-image-builder is updated
+# to work with rpm-ostree based distros
+# see: https://github.com/ublue-os/image-template/issues/60
+if [[ ! -d /usr/libexec/rpm-ostree/wrapped ]]; then
+    echo "cliwrap is not setup, skipping..."
+    exit 0
+fi
+
+# Remove wrapped binaries
+rm -f \
+    /usr/bin/yum \
+    /usr/bin/dnf \
+    /usr/bin/kernel-install
+
+# binaries which were wrapped
+mv -f /usr/libexec/rpm-ostree/wrapped/* /usr/bin
+rm -rf /usr/libexec/rpm-ostree
+
+# Install dnf5 if not present
+if [[ "$(rpm -E %fedora)" -lt 41 ]]; then
+    rpm-ostree install --idempotent dnf5
+    if [[ ! "${IMAGE}" =~ ucore ]]; then
+        dnf5 install -y dnf5-plugins
+    fi
+fi
