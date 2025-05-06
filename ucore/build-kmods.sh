de75379808fc74060cdc261f3a45142b8b932cdd
Krzysztof Gutkowski <krzysio.gutkowski@gmail.com>
2025-05-06 20:21:59 +0200

    chore: build ucore images from ucore-minimal

--- ucore/build-kmods.sh
+++ ucore/build-kmods.sh
@@ -7,5 +7,5 @@ mkdir -p kmods/
 podman run -it --rm \
     -v ./build-files/kmods/:/ctx \
     -v ./kmods:/kmods \
-    ghcr.io/ublue-os/ucore:stable \
+    ghcr.io/ublue-os/ucore-minimal:stable \
     /bin/bash -c "/ctx/build-kmod-gasket.sh"
