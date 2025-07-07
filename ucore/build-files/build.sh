6699574d7a15abe2767cf93ff2ccf41bbdc4dc12
Krzysztof Gutkowski <krzysio.gutkowski@gmail.com>
2025-07-07 21:25:57 +0200

    chore(ucore): stop including gasket kmod

--- ucore/build-files/build.sh
+++ ucore/build-files/build.sh
@@ -2,19 +2,11 @@
 
 set -ouex pipefail
 
-RELEASE="$(rpm -E '%fedora')"
-
-curl -o /etc/yum.repos.d/_copr-gasket.repo https://copr.fedorainfracloud.org/coprs/liquidpl/gasket/repo/fedora-${RELEASE}/liquidpl-gasket-fedora-${RELEASE}.repo
-cp /ctx/rancher-k3s-common.repo /etc/yum.repos.d/
-
-rpm-ostree install -y /ctx/built-kmods/*.rpm \
+rpm-ostree install -y \
     htop \
     ethtool \
     ipset \
     conntrack-tools
-
-rm /etc/yum.repos.d/_copr-gasket.repo \
-   /etc/yum.repos.d/rancher-k3s-common.repo
 
 # TODO: consider moving this to the Ignition config once building
 # disk images from bootc based CoreOS images is working
